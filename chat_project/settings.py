270e3d0aa1af1d38b171b634dc85fbc8f21e8802
eawd123wadd4354as243df <132335250+eawd123wadd4354as243df@users.noreply.github.com>
2025-03-14 08:44:19 +0100

    added channels

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -30,7 +30,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+ASGI_APPLICATION = "chat_project.asgi.application"
+
+
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
