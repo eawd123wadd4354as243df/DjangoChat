f0b2eb732e2f004fda1a7d027d1adcbdfbab8103
eawd123wadd4354as243df <132335250+eawd123wadd4354as243df@users.noreply.github.com>
2025-03-14 12:48:43 +0100

    added user login to connecting via websocket
    # Conflicts:
    #	requirements.txt

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,56 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    user: User
 
-    def disconnect(self, close_code):
-        pass
 
-    def receive(self, text_data):
+    async def connect(self):
+        self.user = self.scope['user']
+
+        if not self.user.is_authenticated:
+            print("user not authenticated")
+            return
+
+        print('user: ', self.user)
+        await self.accept()
+
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        # Send message to room group
+        self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
+
+
+@database_sync_to_async
+def get_user(user_id: int):
+    user = User.objects.get(pk=user_id)
+
+    return user
+
+
+@database_sync_to_async
+def get_name():
+    return User.objects.all()[0].name
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(User, through='Member')
+
+
+class Member(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    date_joined = (models.DateTimeField(auto_now_add=True))
+
+
+
+
+class Message(models.Model):
+    member = models.ForeignKey(Member, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_added',)
